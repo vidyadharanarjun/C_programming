66f5c30a079c12dd23711f7855d19237c41c3d13
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-07-28 14:58:35 -0700

    Add count program

---
+++ control_stuctures/count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n, count = 0;
+	printf("Enter a  number");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		n = n/10;
+		count ++;
+	}
+	printf("Number of digit: %d",count);
+	return 0;
+}
+
