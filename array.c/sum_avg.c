3135102f9d506a83fbea1edfd2b306bab3dddbc3
Vidyadharan Arjun RK <vidyadharanarjun6@gmail.com>
2026-07-29 04:29:36 -0700

    Sum_avg.c

---
+++ array.c/sum_avg.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, sum = 0;
+    float average;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    average = (float)sum / n;
+
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f\n", average);
+
+    return 0;
+}
