ef0751612ad290fbc6d88262a15faca2d3e56322
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-07-28 18:15:20 -0700

    reverse_array

---
+++ array.c/reverse_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+	int arr[10],n,i;
+
+	printf("Enter the number of elements: ");
+	scanf("%d", &n);
+	printf("Enter  the elements:\n");
+
+	for(i=0; i<n;i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("Reverse array:\n");
+	
+for(i=n-1; i>=0; i--)
+{
+	printf("%d", arr[i]);
+}
+	return 0;
+}
