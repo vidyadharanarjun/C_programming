d143d58ae7519a0f8a1bd9612413adf21569e0d5
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-08-12 15:46:24 -0700

    Add integerr.c

---
+++ C_practice/integerr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+    // Declaring integer
+    int x = 5;
+
+    // Printing values
+    printf("Printing Integer value %d", x);
+    return 0;
+}
