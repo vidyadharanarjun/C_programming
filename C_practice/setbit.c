22e4010e79fd43989ed9561a98ad842c7e7c2014
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-08-20 17:01:05 -0700

    Add setbit.c

---
+++ C_practice/setbit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n, i, count = 0;
+printf("Enter a non negative integer: ");
+scanf("%d", &n);
+while(n % 2 == 0)
+{
+printf("1s bit:" );
+}
+if(n / 2)
+{
+if(n == 0)
+printf("Total bits: ");
+}
+return 0;
+}
+
+
