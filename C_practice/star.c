7fb9ef9b32cc955dbfc6da184d2db9819ec88618
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-08-27 14:46:06 -0700

    star.c

---
+++ C_practice/star.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int n, i, j;
+	printf("Enter n value: ");
+	scanf("%d", &n);
+
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < i; j++)
+		{
+			printf("* ");
+		}
+				printf("\n");
+		}
+	}
+
+
+
