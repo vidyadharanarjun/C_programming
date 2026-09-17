b18864bb9199525c6700de07ed66b6244a94228d
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-09-17 09:38:19 -0700

    Add num.c

---
+++ C_practice/num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int i;
+
+    printf("Enter 5 numbers:\n");
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
