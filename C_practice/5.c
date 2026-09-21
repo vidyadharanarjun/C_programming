aaf85fe9bbcbb04f2ee56acd6b67682224629474
vidyadharanarjun <vidyadharanarjun6@gmail.com>
2026-09-20 18:00:08 -0700

    Add 5.c

---
+++ C_practice/5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int i;
+
+    printf("Enter 5 numbers:\n");
+
+    // Input array elements
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Display array elements
+    printf("Array elements are:\n");
+
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
